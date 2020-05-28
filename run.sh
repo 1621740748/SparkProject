441b2018c67a09921bf3ada168d851a614959f45
huangyan <1621740748@qq.com>
2020-05-28 15:15:42 +0800

    提交

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 scp target/SparkProject-1.0-SNAPSHOT.jar home:/data/temp/SparkProject-1.0-SNAPSHOT.jar
-echo "-------------"
 ssh home sudo docker cp  /data/temp/SparkProject-1.0-SNAPSHOT.jar  v2_master_1:/
-echo "*****************"
 ssh home "sudo docker exec -i v2_master_1 spark-submit --class org.sunny.sparkApp /SparkProject-1.0-SNAPSHOT.jar"
-echo "---------------"
 
 
