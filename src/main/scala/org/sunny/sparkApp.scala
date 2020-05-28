7e860d255fc64b7a8f845296495ff5a3426dab36
huangyan <1621740748@qq.com>
2020-05-28 15:13:17 +0800

    提交

--- src/main/scala/org/sunny/sparkApp.scala
+++ src/main/scala/org/sunny/sparkApp.scala
@@ -76,7 +76,7 @@ object sparkApp{
       case x if x>3 => true
       case x if x<=3 => false
     }
-    val rdd3 = sc.parallelize(List(1,2,3,4,5,7,8,9,11))
+    val rdd3 = sc.parallelize(List(1,2,3,4,5,7,8,9,11,13))
     val res = rdd3.groupBy(my)
     res.foreach(println)
     //取出RDD中某一个元素
@@ -84,7 +84,7 @@ object sparkApp{
     val rddList = res.take(num.toInt) //将RDD转为Array
 
     //keyBy：为数据集中每个个体数据增加一个key，形成键值对
-    val rdd4 = sc.parallelize(List(1,2,3,4,5,7,8,9,11))
+    val rdd4 = sc.parallelize(List(1,2,3,4,5,7,8,9,11,13,15))
     val res2 = rdd4.keyBy(_*2)
 
     val res3 = res2.sortBy(x=>x._1,false) //或者直接使用
