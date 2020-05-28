ee9c816359f10f1c6389a8699762a12a199954f7
huangyan <1621740748@qq.com>
2020-05-28 15:25:00 +0800

    提交

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scp target/SparkProject-1.0-SNAPSHOT.jar home:/data/temp/SparkProject-1.0-SNAPSHOT.jar
 ssh home sudo docker cp  /data/temp/SparkProject-1.0-SNAPSHOT.jar  v2_master_1:/
-ssh home "sudo docker exec -i v2_master_1 spark-submit --class org.sunny.sparkApp /SparkProject-1.0-SNAPSHOT.jar"
+ssh home "sudo docker exec -i v2_master_1 spark-submit --class org.sunny.wordCount /SparkProject-1.0-SNAPSHOT.jar"
 
 
---
+++ src/main/scala/org/sunny/wordCount.scala
@@ -0,0 +1,23 @@
+package org.sunny
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.storage.StorageLevel
+
+import scala.util.Sorting
+object wordCount{
+  print("hellp")
+
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org.apache").setLevel(Level.ERROR)
+    val conf = new SparkConf().setAppName("mengapp")
+      .setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val words = sc.textFile("hdfs://localhost:9000/user/hduser/words")
+    val wordsFlatMap = words.flatMap(_.split("\\W+"))
+    val wordsMap = wordsFlatMap.map( w => (w,1))
+    val wordCount = wordsMap.reduceByKey( (a,b) => (a+b))
+    val wordCountSorted = wordCount.sortByKey(true)
+    wordCountSorted.collect.foreach(println)
+
+  }
+}
